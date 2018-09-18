9b174a307d08ae37c8ceeb6bf85d30ab0f7c6135
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-17 17:44:19 -0700

    Copyright added.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+// BW Color Contrast AI - scripts.js
+// Author: Eric Liang
+// Author URI: https://www.eric-liang.com
+// Author Github URI: https://www.github.com/ewliang
+// Project Repository URI: https://github.com/ewliang/BW-Color-Contrast-AI
+// Description: The core logic that handles the BW Color Contrast AI functionalities.
+// License: GPL v3.0
+
 var net = new brain.NeuralNetwork();
 
 var blackBox = document.getElementById('blackBox');
