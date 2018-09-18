9fe8c87604ddc17e39bec559cbcc91165026847b
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-17 19:01:02 -0700

    Updated with simple brain.js code so that training occurs when checkbox is checked. Once unchecked, that's when training data is pushed into the train() function and predictions will start getting generated for the next rounds.

--- scripts.js
+++ scripts.js
@@ -10,30 +10,73 @@ var net = new brain.NeuralNetwork();
 
 var blackBox = document.getElementById('blackBox');
 var whiteBox = document.getElementById('whiteBox');
-
 var toggleTraining = document.getElementById('toggleTraining');
 
 var r, g, b;
 
-r = Math.random(),
-g = Math.random(),
-b = Math.random();
+var trainingData = [];
 
-changeColor();
+var predictedOutput;
 
-console.log(r, g, b);
+// Initialize Colors
+updateColors();
+console.log(Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255));
+
+// Initialize Training Data
+trainingData.push({ input: { r: 0, g: 0, b: 0 }, output: { white: 1 } });
+trainingData.push({ input: { r: 1, g: 1, b: 1 }, output: { black: 0 } });
 
 blackBox.onclick = function() {
-  changeColor();
-}
-whiteBox.onclick = function() {
-  changeColor();
+  if(toggleTraining.checked == true) {
+    trainingData.push({ input: { r: r, g: g, b: b }, output: { black: 0 } });
+  } else {
+    predictedOutput = net.run({ r: r, g: g, b: b });
+    if (predictedOutput.black > predictedOutput.white) {
+      blackBox.style.border = "5px solid black";
+      whiteBox.style.border = "0px";
+    } else {
+      blackBox.style.border = "0px";
+      whiteBox.style.border = "5px solid black";
+    }
+    console.log(predictedOutput);
+  }
+  updateColors();
+  displayTrainingData();
 }
 
-function changeColor() {
+whiteBox.onclick = function() {
+  if(toggleTraining.checked == true) {
+    trainingData.push({ input: { r: r, g: g, b: b }, output: { white: 1 } });
+  } else {
+    predictedOutput = net.run({ r: r, g: g, b: b });
+    if (predictedOutput.black > predictedOutput.white) {
+      blackBox.style.border = "5px solid black";
+      whiteBox.style.border = "0px";
+    } else {
+      blackBox.style.border = "0px";
+      whiteBox.style.border = "5px solid black";
+    }
+    console.log(predictedOutput);
+  }
+  updateColors();
+  displayTrainingData();
+}
+
+toggleTraining.onclick = function() {
+  if(toggleTraining.checked == false) {
+    net.train(trainingData);
+  }
+  // console.log("PREDICTION: " + net.run({ r: r, g: g, b: b }));
+}
+
+function updateColors() {
   r = Math.random();
   g = Math.random();
   b = Math.random();
-  blackBox.style.backgroundColor = 'rgb(' + r * 255 + ', ' + g * 255 + ', ' + b * 255 + ')';
-  whiteBox.style.backgroundColor = 'rgb(' + r * 255 + ', ' + g * 255 + ', ' + b * 255 + ')';
+  blackBox.style.backgroundColor = 'rgb(' + Math.floor(r * 255) + ', ' + Math.floor(g * 255) + ', ' + Math.floor(b * 255) + ')';
+  whiteBox.style.backgroundColor = 'rgb(' + Math.floor(r * 255) + ', ' + Math.floor(g * 255) + ', ' + Math.floor(b * 255) + ')';
+}
+
+function displayTrainingData() {
+  console.log(trainingData);
 }
