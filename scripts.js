6cd628f38ccf1faa218915d41753e3d2e7c9f358
DESKTOP-DNMRE55\Eric Liang <ericwliang94@gmail.com>
2018-09-17 17:41:02 -0700

    Initial commit.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
+var net = new brain.NeuralNetwork();
 
+var blackBox = document.getElementById('blackBox');
+var whiteBox = document.getElementById('whiteBox');
+
+var toggleTraining = document.getElementById('toggleTraining');
+
+var r, g, b;
+
+r = Math.random(),
+g = Math.random(),
+b = Math.random();
+
+changeColor();
+
+console.log(r, g, b);
+
+blackBox.onclick = function() {
+  changeColor();
+}
+whiteBox.onclick = function() {
+  changeColor();
+}
+
+function changeColor() {
+  r = Math.random();
+  g = Math.random();
+  b = Math.random();
+  blackBox.style.backgroundColor = 'rgb(' + r * 255 + ', ' + g * 255 + ', ' + b * 255 + ')';
+  whiteBox.style.backgroundColor = 'rgb(' + r * 255 + ', ' + g * 255 + ', ' + b * 255 + ')';
+}
